d9d31020619110523570778035c8472741f51e7d
Malone Ingham <123647836+MelonCaully@users.noreply.github.com>
2026-05-16 20:08:34 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> d;
+        for (int num : nums) {
+            if (d.count(num) >= 1) {
+                return true;
+            } else { d.insert(num); }
+        }
+        return false;
+    }
+};
